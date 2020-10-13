d9caf814ae690a797c418af15e02ac1c8b157ac1
Kinugawa-Naoki <naoki.kinugawa@gmail.com>
2020-10-13 22:41:04 +0900

    forms.pyを作った

--- main/forms.py
+++ main/forms.py
@@ -2,10 +2,8 @@ from django import forms
 from django.urls import timezone
 
 class CreateDeliverableInfo(forms.Form):
-    name = forms.CharField(max_length=100)
-    git_url = forms.URLField()
-    category = forms.CharField(max_length=100)
-    languages = forms.CharField(max_length=50)
-    option = forms.CharField(max_length=100, null=True, blank=True)
-    created_dt = forms.DateTimeField(default=timezone.now)
-    modified_dt = forms.DateTimeField(null=True, blank=True)
+    name = forms.CharField(max_length=100, label='成果物名')
+    git_url = forms.URLField(label='Git URL')
+    category = forms.CharField(max_length=100, label='カテゴリー')
+    languages = forms.CharField(max_length=50, label='使用言語')
+    option = forms.CharField(max_length=100, null=True, blank=True, label='追記情報')
