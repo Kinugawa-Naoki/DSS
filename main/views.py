88ec4c0cf8b200e618c63eeb2361bf91876be5d9
Kinugawa-Naoki <naoki.kinugawa@gmail.com>
2020-10-30 12:12:16 +0900

    test: test passed -> add view posted comment func test: add -> print() to check error

--- main/views.py
+++ main/views.py
@@ -28,6 +28,7 @@ def deliverable_detailfunc(request, pk):
                 comment_query = PablicComment.objects.filter(deliverable_id__id__iexact=pk).all()
                 return render(request, 'deliverable_detail.html', {'detail_query':detail_query, 'comment_query':comment_query, 'comment_form':form})
             except:
+                print('printed?')
                 return render(request, 'deliverable_detail.html', {'detail_query':detail_query, 'comment_form':form})
         except:
             return redirect('deliverable_detail')
